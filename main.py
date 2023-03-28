1cd8eeef0338bc5776bf8fdf3125e2369e785778
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2023-03-28 14:10:40 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+if __name__ == '__main__':
+        print("Welcome to RoboSpeaker 1.0. Created by DEEPAK JOSHI")
+        while(True):
+                str = input("Enter text to speak : ");
+                if(str == "bye"):
+                        break
+                command = f"  PowerShell -Command \"Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{str}');\""
+                textToSpeak = f"{command}  "
+                os.system(command)
+
+
+
